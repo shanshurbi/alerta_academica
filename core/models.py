adbbbdccab77f2c94d9d6e2a3914f7adaed7d1f0
shanshurbi <rod.urbina@duocuc.cl>
2025-11-26 22:31:16 -0300

    Implemented Estudiante and Alerta database models.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    promedio = models.FloatField()
+    asistencia = models.FloatField()  # Se asume que es un valor entre 0.0 y 1.0
+    tareas_entregadas = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Alerta(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    riesgo = models.FloatField()  # Predicción entre 0 y 1
+    mensaje = models.CharField(max_length=255)
+    fecha = models.DateTimeField(auto_now_add=True) # auto_now_add cumple el requisito de guardar la fecha
+
+    def __str__(self):
+        return f"Alerta para {self.estudiante.nombre} - Riesgo: {self.riesgo:.2f}"
